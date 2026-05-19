72c193aa90866f7e3ebd7cca8e9d9af8095a55d3
TheRealPuma <therealpuma2024@gmail.com>
2026-05-19 11:07:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,200 @@
+import account from './account';
+import character from './character';
+import antispam from './antispam';
+import main from './main';
+import hud from './hud';
+import house from './house';
+import blips from './blips';
+import vehicles from './vehicles';
+import interactions from './interactions';
+import shops from './shops';
+import inventory from './inventory';
+import items from './inventory/index';
+import jobs from './jobs';
+import carshop from './carshop';
+import carWash from './carWash';
+import testdrive from './testdrive';
+import rent from './rent';
+import factionVehicles from './factionVehicles';
+import appearance from './appearance';
+import casino from './casino';
+// import { casinoRoulette } from './casino/server/games/roulette';
+import tuning from './tuning';
+import discord from './discord';
+import donate from './donate';
+// import { casinoCashier } from './casino/server/cashier';
+import bank from './bank';
+import safetyBelt from './safetyBelt';
+import scrapyard from './scrapyard';
+import cruiseControl from './cruiseControl';
+import player from './player';
+import chat from './chat';
+import interaction from './interaction/interaction';
+import admin from './admin';
+import quests from './quests';
+import menu from './menu';
+import phone from './phone';
+import { animations } from './animations/animations';
+import randomQuests from './randomQuests';
+import { gameEvents } from './gameEvents';
+import death from './death';
+import offers from './offers';
+import { faction } from './faction/faction';
+import { web } from './web/web';
+import handling from './handling';
+import crash from './crash';
+import jail from './jail';
+import cityhall from './cityhall';
+import licenses from './licenses';
+import gunClub from './gunClub';
+import carjacker from './carjacker';
+import colors from './colors';
+import workshop from './workshop';
+import dispatch from './dispatch';
+import greenCourier from './greenCourier';
+import adminMP from './adminMP';
+import trading from './trading';
+import fishing from './jobs/fishing/fishing';
+import referral from './referral';
+import buyer from './buyer/buyer';
+import { gasStation } from './gasStation/gasStation';
+import cayoPerico from './cayoPerico';
+import spawnSelector from './spawnSelector';
+import seller from './seller';
+import party from './party';
+import xmas from './xmas';
+import musicPlayer from './musicPlayer';
+import games from './games/games';
+import flappyBird from './flappyBird';
+import grandmaster from './grandmaster';
+import walkieTalkie from './walkieTalkie';
+import drone from './drone';
+import elevators from './elevators';
+import shootingRange from './shootingRange';
+import prism from './prism';
+import diving from './diving';
+import garbage from './garbage';
+import drivingSchool from './drivingSchool';
+import drivingLicenses from './drivingLicenses';
+import business from './business/business';
+import furnitureEditor from './furnitureEditor/furnitureEditor';
+import keyBinds from './keyBinds/keyBinds';
+import taxi from './taxi/taxi';
+import gym from './gym';
+import achievements from './achievements';
+import realtor from './realtor';
+import diceGame from './diceGame';
+import offcies from './offcies';
+import warehouses from './warehouses';
+import organizationRegistration from './organizationRegistration';
+import organizations from './organizations';
+import tennis from './tennis';
+import trade from './trade';
+import battlePass from './battlePass';
+import vehicleInfo from './vehicleInfo';
+import carInsurance from './carInsurance';
+import marriage from './marriage';
+import canteen from './canteen';
+
+export default {
+    account,
+    antispam,
+    character,
+    main,
+    hud,
+    house,
+    blips,
+    vehicles,
+    jobs,
+    interactions,
+    shops,
+    items,
+    inventory,
+    carshop,
+    carWash,
+    testdrive,
+    rent,
+    factionVehicles,
+    appearance,
+    casino,
+    // casinoRoulette,
+    tuning,
+    discord,
+    donate,
+    // casinoCashier,
+    bank,
+    safetyBelt,
+    scrapyard,
+    cruiseControl,
+    player,
+    chat,
+    admin,
+    interaction,
+    quests,
+    menu,
+    phone,
+    animations,
+    randomQuests,
+    gameEvents,
+    handling,
+    death,
+    offers,
+    faction,
+    web,
+    crash,
+    jail,
+    cityhall,
+    licenses,
+    gunClub,
+    carjacker,
+    colors,
+    workshop,
+    dispatch,
+    greenCourier,
+    adminMP,
+    trading,
+    fishing,
+    referral,
+    buyer,
+    gasStation,
+    cayoPerico,
+    seller,
+    party,
+    spawnSelector,
+    xmas,
+    musicPlayer,
+    games,
+    flappyBird,
+    grandmaster,
+    walkieTalkie,
+    drone,
+    elevators,
+    shootingRange,
+    diving,
+    garbage,
+    prism,
+    drivingSchool,
+    drivingLicenses,
+    business,
+    furnitureEditor,
+    keyBinds,
+    taxi,
+    gym,
+    achievements,
+    realtor,
+    diceGame,
+    offcies,
+    warehouses,
+    organizationRegistration,
+    organizations,
+    tennis,
+    trade,
+    battlePass,
+    vehicleInfo,
+    carInsurance,
+    marriage,
+    canteen,
+};
+
+
+
